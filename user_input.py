22f27796fc0f7d3c3441d6f636c44de6efeb76a7
ntoiti <fidesluckia36@gmail.com>
2024-02-25 10:41:05 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("what is your name? ")
+age = input("how old are you? ")
+location = input("what is your location? ")
+print("hello {},you are {} years old and you live in {}.". format(name,age,location))
